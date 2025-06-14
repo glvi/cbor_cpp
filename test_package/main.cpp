afd5b8658eeb3837a4b9b78fd7fa6c470832d3a1
Kai Lothar John <l.john@glvi.de>
2025-06-14 13:08:12 +0200

    Initial commit

---
+++ test_package/main.cpp
@@ -0,0 +1,12 @@
+#include "glvi_cbor.h"
+#include <vector>
+#include <string>
+
+int main() {
+    glvi_cbor();
+
+    std::vector<std::string> vec;
+    vec.push_back("test_package");
+
+    glvi_cbor_print_vector(vec);
+}
